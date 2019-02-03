24e3b233129858c76aa3355b76056c29880f6b9f
Julio Hurtado Jerves <juliens@outlook.com>
2019-02-03 15:23:49 -0500

    First version of plugin

---
+++ inc/plugins/gist.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * MyGist
+ * 
+ * MIT License
+ * 
+ * Copyright (c) 2019 Julio Hurtado Jerves
+ */
+
+$plugins->add_hook("parse_message", "gist_run");
+
+if (!defined("IN_MYBB")) {
+    die('Direct initialization of this file is not allowed.<br /><br />Please make sure IN_MYBB is defined.');
+}
+function gist_info()
+{
+    return array(
+        "name" => "MyGist",
+        "description" => "Embed gist in posts ",
+        "website" => "https://ilvermorny.xyz",
+        "author" => "Juliens",
+        "authorsite" => "ttps://ilvermorny.xyz",
+        "version" => "1.0",
+        "guid" => "",
+        "compatibility" => "18*"
+    );
+}
+function gist_activate()
+{
+}
+
+function gist_deactivate()
+{
+}
+function gist_run($message)
+{
+    $message = preg_replace(
+        '#\[gist\]https:\/\/gist\.github\.com\/(.*)\/([a-zA-Z0-9]+)\[\/gist\]#i',
+        '<script src="https://gist.github.com/$1/$2.js"></script>',
+        $message
+    );
+    return $message;
+}
+
+?>
